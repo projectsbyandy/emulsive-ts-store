903ceba67d98df03713a37db443a534aef9bbe4a
Andrew Chang <mrchang@outlook.com>
2025-01-20 17:31:24 +0000

    Added hero section to landing page

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -1,6 +1,38 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from '@/components/ui/carousel';
+
+
+import hero1 from '../assets/hero1.webp';
+import hero2 from '../assets/hero2.webp';
+import hero3 from '../assets/hero3.webp';
+import hero4 from '../assets/hero4.webp';
+import hero5 from '../assets/hero5.webp';
+import { CardContent } from './ui/card';
+
+const carouselImages = [hero1, hero2, hero3, hero4, hero5];
+
 function HeroCarousel() {
   return (
-    <div>HeroCarousel</div>
+    <div className='hidden lg:block'>
+      <Carousel>
+        <CarouselContent>
+          {carouselImages.map((image, index) => {
+            return <CarouselItem key={index}>
+              <CardContent className='p-2'>
+                <img src={image} alt='hero' className='w-full h-[24rem] rounded-md object-cover' />
+              </CardContent>
+            </CarouselItem>
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
   )
 }
 export default HeroCarousel
