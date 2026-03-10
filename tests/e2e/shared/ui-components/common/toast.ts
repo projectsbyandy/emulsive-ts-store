d1c9dd8290efd8e7cf7a2e8040c95285f2c8da4c
Andrew Chang <mrchang@outlook.com>
2026-03-10 11:12:11 +0000

    bumped packages. updated toast locator

--- tests/e2e/shared/ui-components/common/toast.ts
+++ tests/e2e/shared/ui-components/common/toast.ts
@@ -2,5 +2,5 @@ import { Page } from "@playwright/test";
 import { hasContentLoaded } from "@e2e-shared/playwright-helpers";
 
 export const hasToastAppeared = async (message: string, page: Page) : Promise<boolean> => {
-  return await hasContentLoaded(page.getByRole('status').locator(`div:text("${message}")`));
+  return await hasContentLoaded(page.locator(`div:text("${message}")`));
 }
